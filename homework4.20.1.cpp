c91f1c2a74ef5e89a6a3d5b1d8ec14d5c4fd453d
sudo <meshackmasam@gmail.com>
2020-04-22 19:16:10 +0800

    2. card game 1. Divisible by 3 between 1 - 30

--- homework4.20.1.cpp
+++ homework4.20.1.cpp
@@ -1,7 +1,16 @@
 //working on it.
 //using while loop to output numbers which are divisable by 3 from 1 to 30
-//Do it yourself
 #include <iostream>
 int main(){
+    int min,max;
+    min = 1;
+    max = 30;
+    while(min <= max){
+        if(min % 3 == 0){
+            std::cout << min << std::endl;
+        }
+        min++;
+    }
+
     return 0;
 }
--- homework4.20.2.cpp
+++ homework4.20.2.cpp
@@ -1,7 +1,23 @@
 //am thinking of a card game
-//do it yourself
 #include <iostream>
+#include <time.h>
 int main(){
+    char cards[] = {'2','3','4','5','6','7','8','9','D','J','Q','K','A'};
+    char card = ' ';
+    char choice = ' ';
+    int random_card_number = 0;
+    //pick random card from array of cards
+    srand(time(NULL));
+    random_card_number = rand() % 13 + 1;
+    card = cards[random_card_number];
 
+    std::cout << "I'm thinking of a card!\n";
+    do {
+        std::cout << "Choose a card between (2 to 9, D, J,Q, K, A) \n";
+        std::cin >> choice;
+        if(choice != card)
+            std::cout << "Wrong Card!\n";
+    }while(card != choice);
+    std::cout << "Congratulations! The answer is " << card << "! \n";
     return 0;
 }

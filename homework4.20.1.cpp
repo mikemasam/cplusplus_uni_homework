f6ffffb87d38e6e88e7470eb17f8e317d469556e
sudo <meshackmasam@gmail.com>
2020-04-22 17:58:51 +0800

    new homework

---
+++ homework4.20.1.cpp
@@ -0,0 +1,7 @@
+//working on it.
+//using while loop to output numbers which are divisable by 3 from 1 to 30
+//Do it yourself
+#include <iostream>
+int main(){
+    return 0;
+}
---
+++ homework4.20.2.cpp
@@ -0,0 +1,7 @@
+//am thinking of a card game
+//do it yourself
+#include <iostream>
+int main(){
+
+    return 0;
+}
